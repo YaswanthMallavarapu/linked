1402aeb11f985aa5b02b900176708c93b8b02961
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2025-02-03 21:27:10 +0530

    two solutions added

---
+++ LengthOfLoop.java
@@ -0,0 +1,36 @@
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+// Function should return the length of the loop in LL.
+
+class Solution {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null)
+            return 0;
+        slow = slow.next;
+        int cnt = 1;
+        while (slow != fast) {
+            slow = slow.next;
+            cnt++;
+        }
+        return cnt;
+    }
+}
